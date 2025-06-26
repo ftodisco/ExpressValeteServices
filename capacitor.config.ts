2a600c2b65d26077085cbfb816d9155f0dd34730
ftodisco <ftodisco15@gmail.com>
2025-06-26 01:21:13 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+
+import { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.9d163e31fa3b4b219ca1d304089c9805',
+  appName: 'stitch-and-shuttle-app',
+  webDir: 'dist',
+  server: {
+    url: 'https://9d163e31-fa3b-4b21-9ca1-d304089c9805.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  },
+  bundledWebRuntime: false
+};
+
+export default config;
