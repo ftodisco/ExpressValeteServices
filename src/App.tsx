66cfdebb2a03d1bb967dbeac5d200423a91b9c52
ftodisco <ftodisco15@gmail.com>
2025-07-14 14:42:59 -0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+index.html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Express Valete Services</title>
+</head>
+<body style="font-family: sans-serif; text-align: center; padding-top: 100px;">
+  <h1>🚗 Express Valete Services</h1>
+  <p>Coming Soon...</p>
+</body>
+</html>
